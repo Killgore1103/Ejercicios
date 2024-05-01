3d4b429f4022a3b2836aae46e7b88c41b68d55eb
Gonzalo Ochoa <148717336+Killgore1103@users.noreply.github.com>
2024-05-01 17:49:59 -0300

    Add files via upload

---
+++ Calculadora de formas/Program.cs
@@ -0,0 +1,36 @@
+﻿using Clases;
+using System.Text;
+
+namespace Calculadora_de_formas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Figura> figuras = new List<Figura> ();
+
+            Cuadrado cuadrado = new Cuadrado(12f);
+            Circulo circulo = new Circulo(9f);
+            Rectangulo rectangulo = new Rectangulo(5f, 7f);
+
+            figuras.Add(cuadrado);
+            figuras.Add(circulo);
+            figuras.Add(rectangulo);
+
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach(Figura figura in figuras)
+            {
+                sb.AppendLine($"Tipo: {figura.GetType().ToString()}");
+                sb.AppendLine($"{figura.Dibujar()}");
+                sb.AppendLine($"Perimetro: {Math.Round(figura.CalcularPerimetro(), 2).ToString()}");
+                sb.AppendLine($"Superficie: {Math.Round(figura.CalcularSuperficie(), 2).ToString()}");
+                sb.AppendLine("------------------------------------------------------------");
+            }
+
+            Console.WriteLine(sb.ToString());
+
+        }
+    }
+}
---
+++ Figura.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public abstract class Figura
+    {
+        public virtual string Dibujar()
+        {
+            return "Dibujando forma";
+        }
+
+        public abstract double CalcularSuperficie();
+
+        public abstract double CalcularPerimetro();
+    }
+
+    public class Rectangulo : Figura
+    {
+        public float lado1;
+        public float lado2;
+
+        public Rectangulo(float lado1, float lado2)
+        {
+            this.lado1 = lado1;
+            this.lado2 = lado2;
+        }
+
+
+        public override string Dibujar()
+        {
+            return "Dibujando rectangulo";
+        }
+
+        public override double CalcularSuperficie()
+        {
+            return lado1 * lado2;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return (lado1 * 2) + (lado2 * 2);
+        }
+    }
+
+    public sealed class Circulo : Figura
+    {
+        float radio;
+
+        public Circulo(float radio)
+        {
+            this.radio = radio;
+        }
+
+        public override string Dibujar()
+        {
+            return "Dibujando circulo";
+        }
+
+        public override double CalcularSuperficie()
+        {
+            return Math.PI * Math.Pow(radio, 2);
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return Math.Pow(Math.PI, 2) * radio;
+        }
+    }
+
+    public sealed class Cuadrado : Rectangulo
+    {
+
+        public Cuadrado(float lado) : base(lado, lado) { }
+
+        public override string Dibujar()
+        {
+            return "Dibujando cuadrado";
+        }
+
+        public override double CalcularSuperficie()
+        {
+            return lado1 * lado2;
+        }
+
+        public override double CalcularPerimetro()
+        {
+            return (lado1 * 2) + (lado2 * 2);
+        }
+    }
+
+}
